405a469c56271493e8e5414b2dc0c91efe4d5cdc
mlaskowski <maciejlaskowsky@gmail.com>
2019-11-19 22:30:56 +0100

    add Marker functionality

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
-// import { User } from './User';
-// import { Company } from './Company'
-import GoogleMapsLoader from 'google-maps'
+import { User } from './User';
+import { Company } from './Company'
+import { CustomMap } from './CustomMap';
 
-// const user = new User();
-// const company = new Company();
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
 
-// console.log(user)
-// console.log(company)
-
-const mapDiv = document.getElementById('map');
-GoogleMapsLoader.KEY = process.env.GOOGLE_MAPS_KEY;
-GoogleMapsLoader.load(function(google) {
-  new google.maps.Map(mapDiv, {
-    zoom: 1,
-    center: {
-      lat: 0,
-      lng: 0
-    }
-  });
-});
+customMap.addMarker(user);
+customMap.addMarker(company);
